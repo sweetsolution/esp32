e121994d54cd7f0a453be2f7a1c6c63a283d5196
Matthias <Suess.Matthias@gmx.de>
2018-12-21 16:12:16 +0100

    LED fading table

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,14 @@ const int ledChannel = 0;
 const int resolution = 8;
 
 
+// LED fading table from https://www.mikrocontroller.net/articles/LED-Fading
+const uint16_t pwmtable_8D[32] PROGMEM =
+{
+    0, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 10, 11, 13, 16, 19, 23,
+    27, 32, 38, 45, 54, 64, 76, 91, 108, 128, 152, 181, 215, 255
+};
+
+
 void setup() {
   Serial.begin(9600);
   ledcSetup(ledChannel, freq, resolution);
@@ -16,13 +24,13 @@ void setup() {
 
 void dimm()
 {
-  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
-    ledcWrite(ledChannel, dutyCycle);
+  for (int dutyCycle = 0; dutyCycle < 32; dutyCycle++) {
+    ledcWrite(ledChannel, pwmtable_8D[dutyCycle]);
     delay(5);
   }
  
-  for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
-    ledcWrite(ledChannel, dutyCycle);
+  for (int dutyCycle = 31; dutyCycle >= 0; dutyCycle--) {
+    ledcWrite(ledChannel, pwmtable_8D[dutyCycle]);
     delay(5);
   }
 
@@ -38,4 +46,3 @@ void loop() {
 
   delay(200);
 }
-
